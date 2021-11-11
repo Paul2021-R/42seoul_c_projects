62e14e46aa14e9c9ed86c81fcc0be1bef6eb9a49
Ryu <ryu.axel@gmail.com>
2021-11-11 22:17:22 +0900

    update

---
+++ Libft/ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 14:49:45 by haryu             #+#    #+#             */
+/*   Updated: 2021/11/11 15:10:03 by haryu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	       size_t	i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			break ;
+		i++;
+	}
+	return ((int)(str1[i] - str2[i]));
+}
