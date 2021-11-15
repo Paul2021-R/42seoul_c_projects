359eb9f65f0a639672e4eed585c3484d359ec11f
haryu <ryu.axel@gmail.com>
2021-11-15 09:05:48 +0900

    update

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 15:20:57 by haryu             #+#    #+#             */
-/*   Updated: 2021/11/12 16:17:12 by haryu            ###   ########.fr       */
+/*   Updated: 2021/11/15 08:36:49 by haryu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 size_t	ft_strcheck(const char *big, const char *little, size_t loc)
 {
-	   int	i;
 	size_t	ret;
+	   int	i;
 
 	i = 0;
 	ret = loc;
