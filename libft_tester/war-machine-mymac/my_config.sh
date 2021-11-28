cc4191d8b1367b3c3ae8c843b48c20de779b85ee
Ryu <ryu.axel@gmail.com>
2021-11-28 22:19:18 +0900

    update & test result

--- libft_tester/war-machine-mymac/my_config.sh
+++ libft_tester/war-machine-mymac/my_config.sh
@@ -5,7 +5,7 @@
 ###################################
 
 #Edit the next line with the path of your libft project.
-PATH_LIBFT=/Users/wildeengineerr/workplace/Cadet-42_C/Libft
+PATH_LIBFT=/Users/wildeengineerr/workspace/Cadet-42_C/Libft
 
 #If you use a relative path, don't put double quotes.
 #For example :
