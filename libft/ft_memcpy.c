85eab6850ccdbd9372cd85f8f5e7882eca4d5486
haryu <ryu.axel@gmail.com>
2021-12-01 14:46:06 +0900

    update ft_memcpy

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,25 +6,27 @@
 /*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 20:00:36 by haryu             #+#    #+#             */
-/*   Updated: 2021/11/30 13:52:36 by haryu            ###   ########.fr       */
+/*   Updated: 2021/12/01 14:45:45 by haryu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
 {
-	size_t	i;
-	char	*tmp_src;
+	size_t			i;
+	unsigned char	*tmp_src;
 
 	i = 0;
 	if (!dest && !src)
 		return (NULL);
-	tmp_src = (char *)src;
+	tmp_src = (unsigned char *)src;
 	while (i < n)
 	{
-		*(char *)(dest + i) = *(tmp_src + i);
-		i++;
+		*(unsigned char *)(dest + i) = *(tmp_src + i);
+		if (i + 1 < n)
+			*(unsigned char *)(dest + i + 1) = *(tmp_src + i + 1);
+		i += 2;
 	}
 	return (dest);
 }
