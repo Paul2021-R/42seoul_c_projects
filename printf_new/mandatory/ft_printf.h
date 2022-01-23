e9c88f7d0a317ab3494811cee8974bd93826369b
haryu <ryu.axel@gmail.com>
2022-01-24 00:10:38 +0900

    delete limits.h on ft_printf.h

--- printf_new/mandatory/ft_printf.h
+++ printf_new/mandatory/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 22:02:37 by haryu             #+#    #+#             */
-/*   Updated: 2022/01/23 23:30:47 by haryu            ###   ########.fr       */
+/*   Updated: 2022/01/24 00:01:15 by haryu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 # include <stdarg.h>
 # include "../libft/libft.h"
-# include <limits.h>
 
 void	ft_putchar(int c, int *len);
 void	ft_putstr(char *s, int *len);
