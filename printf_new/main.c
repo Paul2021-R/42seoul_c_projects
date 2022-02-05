a15fa2181f5af423d387e2893e454e43d4e3137a
haryu <ryu.axel@gmail.com>
2022-02-06 00:04:14 +0900

    update finished

--- printf_new/main.c
+++ printf_new/main.c
@@ -1,10 +1,9 @@
-
 #include "./mandatory/ft_printf.h"
 #include <stdio.h>
 
 int main(void)
 {
-	int len1 = ft_printf("%s", "");
+	int len1 = ft_printf("%m", "");
 	ft_printf("%d\n", len1);
 	return (0);
 }
