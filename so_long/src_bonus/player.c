e7442027bdc83e94ee19077ea9b46d5ffac883e8
haryu <ryu.axel@gmail.com>
2022-03-10 16:21:34 +0900

    1. 전체 구조 재 정리 및 아이맥 컴파일 가능하도록 수정 완료. 2. openGL 버전으로 컴파일 가능하게 만들어야함. 3. 좀더 구성진 구성을 만들 수 있어야 하고 이를 위한 구체적인 형태 구현이 필요함. 4. 우선 플레이어 만들기 및 이동을 위한 구조 파악이 우선 필요해 보임.

---
+++ so_long/src_bonus/player.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   player.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 18:58:34 by haryu             #+#    #+#             */
+/*   Updated: 2022/03/10 16:18:44 by haryu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+void	print_player(t_mlx *vars, t_img *sprite, int x, int y)
+{
+	mlx_put_image_to_window(vars->mlx, vars->mlx_win, sprite[0].img, x, y);
+	return ;
+}
+
+void	player_load(t_mlx *vars, t_player *player, int x, int y)
+{
+	int i;
+
+	i = 0;
+	player = malloc(sizeof(t_player) * 1);
+	player->sprite = malloc(sizeof(t_img *) * 4);
+	while (i < 4)
+	{
+		printf("%d\n", i);
+		player->sprite[i] = malloc(sizeof(t_img) * 1);
+		if (i == 0)
+		{
+			player->sprite[i][0].img = mlx_xpm_file_to_image(vars->mlx, PLAYER_0, &player->sprite[i][0].width, &player->sprite[i][0].height);
+			printf("\n0번 할당 : %p\n",&player->sprite[i]->img);
+		}
+		if (i == 1)
+		{
+			player->sprite[i][0].img = mlx_xpm_file_to_image(vars->mlx, PLAYER_1, &player->sprite[i][0].width, &player->sprite[i][0].height);
+			printf("\n1번 할당 : %p\n",&player->sprite[i]->img);
+		}
+		if (i == 2)
+		{
+			player->sprite[i][0].img = mlx_xpm_file_to_image(vars->mlx, PLAYER_2, &player->sprite[i][0].width, &player->sprite[i][0].height);
+			printf("\n2번 할당 : %p\n",&player->sprite[i]->img);
+		}
+		if (i == 3)
+		{
+			player->sprite[i][0].img = mlx_xpm_file_to_image(vars->mlx, PLAYER_3, &player->sprite[i][0].width, &player->sprite[i][0].height);
+			printf("\n3번 할당 : %p\n",&player->sprite[i]->img);
+		}
+
+		i++;
+	}
+}
+
+int	what_coordinates(char **map, int *x, int *y, int limit)
+{
+	int		i;
+	int  	j;
+	char	*row;
+
+	i = 0;
+	j = 0;
+	limit /= 50;
+	while (i < limit)
+	{
+		row = ft_strrchr(map[i], 'P');
+		if (row != NULL)
+		{
+			*x = (int)(&row[0] - &map[i][0]);
+			j += 1;
+			if (j == 2)
+				break ;
+			*y = i;
+		}
+		i++;
+	}
+	*x *= 50;
+	*y *= 50;
+	if (j != 1)
+		return (1);
+	else 
+		return (0);
+}
+
+void	player_call(t_map *map, t_mlx *vars, t_player *player)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	if (what_coordinates((*map).map_data, &x, &y, (*map).y))
+	{
+		printf("map data가 잘못 되어 있습니다.");
+		exit(1);
+	}
+	player_load(vars, player, x, y);
+	printf("x = %d y = %d\n", x, y);
+	printf("%p\n", &player->sprite[0]);
+	print_player(vars, player->sprite[0], x, y);
+	
+}
