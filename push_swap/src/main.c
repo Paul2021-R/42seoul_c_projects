bf43f3976027e9a3f7ef3af30fb05d58cef5959c
haryu <ryu.axel@gmail.com>
2022-04-18 04:48:50 +0900

    push swap 1. make stack_a push elements to stack_b with lis standard. 2. test ssh server to use ipad for coding

--- push_swap/src/main.c
+++ push_swap/src/main.c
@@ -6,11 +6,43 @@
 /*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 16:14:33 by haryu             #+#    #+#             */
-/*   Updated: 2022/04/16 00:21:01 by haryu            ###   ########.fr       */
+/*   Updated: 2022/04/18 04:46:50 by haryu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pushswap.h"
+
+void	make_stack_a_to_lis_up(t_pushlist **push)
+{
+	int	i;
+	int	j;
+	int check;
+
+	i = 0;
+	while (i < (*push)->lis_len)
+	{
+		check = 0;
+		j = 0;
+		while(j < (*push)->lis_len)
+		{
+			if ((*push)->stack_a->data == (*push)->array[j])
+			{
+				i++;
+				check = 1;
+			}
+			j++;
+		}
+		if (check == 0)
+		{
+			pb(push);
+			//lst_print(push);
+			continue ;
+		}
+		ra(push, 0);
+		//lst_print(push);
+	}
+	return ;
+}
 
 int	main(int ac, char **av)
 {
@@ -26,5 +58,9 @@ int	main(int ac, char **av)
 	fill_stack(&push_swaper, ac);
 	get_lis(&push_swaper);
 	lis_print(&push_swaper);
+	lst_print(&push_swaper);
+	make_stack_a_to_lis_up(&push_swaper);
+	//where_is_lis_a(&push_swaper);
+	lst_print(&push_swaper);
 	return (0);
 }
