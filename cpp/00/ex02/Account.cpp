12c535c7a2a2bc8ac9731711c3152709816fd25f
haryu <haryu@student.42seoul.kr>
2022-09-16 15:38:58 +0900

    [cpp] static function

--- cpp/00/ex02/Account.cpp
+++ cpp/00/ex02/Account.cpp
@@ -6,11 +6,24 @@
 /*   By: haryu <haryu@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 19:44:19 by haryu             #+#    #+#             */
-/*   Updated: 2022/09/14 23:45:38 by haryu            ###   ########.fr       */
+/*   Updated: 2022/09/16 15:36:55 by haryu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <time.h>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+void Account::_displayTimestamp(void) {
+	time_t CurTime = time(NULL);
+	struct tm *pLocal = localtime(&CurTime);
+	std::cout << "[" << pLocal->tm_year << pLocal->tm_mon << pLocal->tm_mday << "_" << pLocal->tm_hour << pLocal->tm_min << pLocal->tm_sec << "] "
+}
 
 Account::Account() {
 
