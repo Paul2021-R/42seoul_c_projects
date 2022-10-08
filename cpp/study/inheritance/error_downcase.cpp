9256ae1e930d3e50f15cad7e2d2f4ec58f19dccb
haryu <haryu@student.42seoul.kr>
2022-10-08 22:50:37 +0900

    [cpp] study inheritance more and study how to use derived class with pointer, override concept, variable keyword

---
+++ cpp/study/inheritance/error_downcase.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+class Base {
+  std::string s;
+
+ public:
+  Base() : s("기반") { std::cout << "기반 클래스" << std::endl; }
+
+  void what() { std::cout << s << std::endl; }
+};
+class Derived : public Base {
+  std::string s;
+
+ public:
+  Derived() : s("파생"), Base() { std::cout << "파생 클래스" << std::endl; }
+
+  void what() { std::cout << s << std::endl; }
+};
+int main() {
+  Base p;
+  Derived c;
+
+  std::cout << "=== 포인터 버전 ===" << std::endl;
+  Base* p_p = &p;
+
+  Derived* p_c = static_cast<Derived*>(p_p);
+  p_c->what();
+
+  return 0;
+}
